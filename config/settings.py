bff846f140a4567b7faeff9dc35ddad9edfcb953
Petru-456 <zephpeterson456@gmail.com>
2026-08-12 10:09:21 +0300

    Configure Django static files for production

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]

aec8e9091ab63d4665e9b3a67d8e08d7712ea0fa
Petru-456 <zephpeterson456@gmail.com>
2026-08-12 03:45:05 +0300

    Remove temporary deployment diagnostic

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-print('DEPLOY ALLOWED_HOSTS:', os.environ.get('DJANGO_ALLOWED_HOSTS'))
 
 ALLOWED_HOSTS = [
     host.strip()

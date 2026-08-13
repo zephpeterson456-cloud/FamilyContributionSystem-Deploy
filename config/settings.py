8fb56806989334af26290e81dcb89d3c3e1051f9
Petru-456 <zephpeterson456@gmail.com>
2026-08-13 21:02:20 +0300

    Configure database URL for deployment

--- config/settings.py
+++ config/settings.py
@@ -89,11 +89,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
